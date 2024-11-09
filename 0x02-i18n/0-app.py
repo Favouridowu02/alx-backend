b62656fd45000a921e6dea8a0b965760401eb618
Favour Idowu <favouridowu02@gmail.com>
2024-11-09 22:17:07 +0100

    Add a file  that renders a template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    This Module contains the Flask app and a  single route
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=True)
+def home():
+    return render_template('0-index.html')
