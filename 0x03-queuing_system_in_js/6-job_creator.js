9b5f4aa6d0288d8cd4197e98549e0fe85b018ac8
Favour Idowu <favouridowu02@gmail.com>
2024-12-06 22:33:45 +0100

    Add a file Create a queue with Kue Create an object containing the Job data with an object containing phoneNumber and message ot type String
    Create a queue named push_notification_code, and create a job with the object created before
    When the job is created without error, log to the console Notification job created: JOB ID
    When the job is completed, log to the console Notification job completed
    When the job is failing, log to the console Notification job failed

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import { createQueue } from 'kue';
+
+const objData = {
+  phoneNumber: '07038575846',
+  message: 'I am Favour',
+};
+
+const push_notification_code = createQueue();
+
+const job = push_notification_code.create('job', objData);
+
+job.save((err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
+
+job.on('complete', () => console.log('Notification job completed')).on('failed', () => console.log('Notification job created'));
